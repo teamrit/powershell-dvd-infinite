7e5d20631a6c8e03a3b08fc099972148994cdecb
Github <amritpalsingha4@gmail.com>
2021-07-14 03:46:05 -0400

    🎉 initial commit

---
+++ runner.ps1
@@ -0,0 +1,49 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+$DeviceWidth = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width;
+$DeviceHeight = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height;
+
+# start from 0, 0 
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(1070, 1070)
+
+$directionX = $true
+$directionY = $false
+
+while ($true) {
+
+  # check if colliding with the edge of the screen
+
+  $Pos = [System.Windows.Forms.Cursor]::Position
+
+  if ($directionX) {
+    $x = $Pos.X + 1
+  }
+  else {
+    $x = $Pos.X - 1
+  }
+  if ($directionY) {
+    $y = $Pos.Y + 1
+  }
+  else {
+    $y = $Pos.Y - 1
+  }
+
+  if ($y -eq 0) {
+    $directionY = $true
+  }
+  if ($x -eq 0) {
+    $directionX = $true
+  }
+  if ($y -eq $DeviceHeight) {
+    $directionY = $false
+  }
+  if ($x -eq $DeviceWidth) {
+    $directionX = $false
+  }
+
+  # set cursor position to the new x and y
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+  
+  # speed of the movement of cursor "per-movement"
+  Start-Sleep -Milliseconds 1
+}
